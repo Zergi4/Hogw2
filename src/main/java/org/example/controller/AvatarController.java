7cd0255d0ebd5a118136c616f1432374af364eff
Serg <ksyssau@ya.ru>
2023-10-16 22:01:51 +0300

    1

---
+++ src/main/java/org/example/controller/AvatarController.java
@@ -0,0 +1,38 @@
+package org.example.controller;
+
+
+import com.example.hwswaggeruiind8.service.AvatarService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@RequestMapping("/avatar")
+public class AvatarController {
+
+    private final AvatarService avatarService;
+
+    public AvatarController(AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+    @PostMapping(value = "/{studentId}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long studentId, @RequestParam MultipartFile avatar) throws IOException {
+        avatarService.uploadAvatar(studentId, avatar);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/student/{id}/avatar-from-db")
+    public ResponseEntity<byte[]> downloadAvatarFromDb(@PathVariable Long id) {
+        return avatarService.downloadAvatarByStudentFromDb(id);
+    }
+
+    @GetMapping(value = "/student/{id}/avatar-from-file")
+    public void downloadAvatarFromFileSystem(@PathVariable Long id, HttpServletResponse response) throws IOException{
+        avatarService.downloadAvatarFromFileSystem(id, response);
+    }
+}
---
+++ src/main/java/org/example/controller/StudentController.java
@@ -0,0 +1,56 @@
+package org.example.controller;
+
+
+import com.example.hwswaggeruiind8.enitity.Faculty;
+import com.example.hwswaggeruiind8.enitity.Student;
+import com.example.hwswaggeruiind8.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public Student create(@RequestBody Student student) {
+        return studentService.add(student.getName(), student.getAge());
+    }
+
+    @GetMapping
+    public Optional<Student> get(@RequestParam long id) {
+        return studentService.get(id);
+    }
+
+    @PutMapping
+    public Student update(@RequestBody Student student) {
+        return studentService.update(student.getId(), student.getName(), student.getAge());
+    }
+
+    @DeleteMapping
+    public Student delete(@RequestParam long id) {
+        return studentService.delete(id);
+    }
+
+    @GetMapping("/by-age")
+    public List<Student> getByAge(@RequestParam int age) {
+        return studentService.getByAge(age);
+    }
+
+    @GetMapping("/by-age-between")
+    public List<Student> getByAgeBetween(@RequestParam int min, @RequestParam int max) {
+        return studentService.getByAgeBetween(min, max);
+    }
+
+    @GetMapping("/faculty-by-student-id")
+    public Faculty getFacultyByStudentId(@RequestParam Long id) {
+        return studentService.getFacultyByStudentId(id);
+    }
+}
